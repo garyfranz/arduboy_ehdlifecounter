6733bbd9935f96d702d362df3311ba2f4e4925b4
Gary Franz <gary.franz@gmail.com>
2017-01-04 22:58:35 -0600

    Changed layout for 8 player pod.
    Still need to add ability to set/change actual values.

--- arduboy_edhlifecounter.ino
+++ arduboy_edhlifecounter.ino
@@ -12,103 +12,40 @@
 #include <Arduboy2.h>
 
 //app states
-static const unsigned char PLAYERSELECTION PROGMEM = 0;
-static const unsigned char VALUESELECTION PROGMEM = 1;
-static const unsigned char VALUEENTRY PROGMEM = 2;
+static const unsigned int PLAYERSELECTION PROGMEM = 0;
+static const unsigned int VALUESELECTION PROGMEM = 1;
 
 //screen selections
-static const unsigned char SEL_PLAYER1 PROGMEM = 1;   
-static const unsigned char SEL_PLAYER2 PROGMEM = 2;   
-static const unsigned char SEL_PLAYER3 PROGMEM = 3;   
-static const unsigned char SEL_PLAYER4 PROGMEM = 4;   
-static const unsigned char SEL_PLAYER5 PROGMEM = 5;   
-static const unsigned char SEL_PLAYER6 PROGMEM = 7;   
+static const unsigned int SEL_PLAYER1 PROGMEM = 0;
+static const unsigned int SEL_PLAYER2 PROGMEM = 1;
+static const unsigned int SEL_PLAYER3 PROGMEM = 2;
+static const unsigned int SEL_PLAYER4 PROGMEM = 3;
+static const unsigned int SEL_PLAYER5 PROGMEM = 4;
+static const unsigned int SEL_PLAYER6 PROGMEM = 5;
+static const unsigned int SEL_PLAYER7 PROGMEM = 6;
+static const unsigned int SEL_PLAYER8 PROGMEM = 7;
 
-static const unsigned char SEL_LIFE PROGMEM = 10;
-static const unsigned char SEL_POISON PROGMEM = 11;
-static const unsigned char SEL_EXPERIENCE PROGMEM = 12;
-static const unsigned char SEL_COMMANDER1 PROGMEM = 13;
-static const unsigned char SEL_COMMANDER2 PROGMEM = 14;
-static const unsigned char SEL_COMMANDER3 PROGMEM = 15;
-static const unsigned char SEL_COMMANDER4 PROGMEM = 16;
-static const unsigned char SEL_COMMANDER5 PROGMEM = 17;
-static const unsigned char SEL_COMMANDER6 PROGMEM = 18;
-
-static const unsigned char SEL_TENS PROGMEM = 20;
-static const unsigned char SEL_ONES PROGMEM = 21;
+static const unsigned int SEL_LIFE PROGMEM = 10;
+static const unsigned int SEL_POISON PROGMEM = 11;
+static const unsigned int SEL_COMMANDER1 PROGMEM = 12;
+static const unsigned int SEL_COMMANDER2 PROGMEM = 13;
+static const unsigned int SEL_COMMANDER3 PROGMEM = 14;
+static const unsigned int SEL_COMMANDER4 PROGMEM = 15;
+static const unsigned int SEL_COMMANDER5 PROGMEM = 16;
+static const unsigned int SEL_COMMANDER6 PROGMEM = 17;
+static const unsigned int SEL_COMMANDER7 PROGMEM = 18;
+static const unsigned int SEL_COMMANDER8 PROGMEM = 19;
 
 Arduboy2 arduboy;
 
-unsigned char buttonAvailable;
+unsigned int buttonAvailable;
 
-unsigned char state;
+unsigned int state;
 
-unsigned char selectionPlayer;
-unsigned char selectionValue;
-unsigned char selectionDigit;
+unsigned int selectionPlayer;
+unsigned int selectionValue;
 
-unsigned char lifeP1;
-unsigned char lifeP2;
-unsigned char lifeP3;
-unsigned char lifeP4;
-unsigned char lifeP5;
-unsigned char lifeP6;
-
-unsigned char poisonP1;
-unsigned char poisonP2;
-unsigned char poisonP3;
-unsigned char poisonP4;
-unsigned char poisonP5;
-unsigned char poisonP6;
-
-unsigned char expP1;
-unsigned char expP2;
-unsigned char expP3;
-unsigned char expP4;
-unsigned char expP5;
-unsigned char expP6;
-
-unsigned char cmddamP1_C1;
-unsigned char cmddamP1_C2;
-unsigned char cmddamP1_C3;
-unsigned char cmddamP1_C4;
-unsigned char cmddamP1_C5;
-unsigned char cmddamP1_C6;
-
-unsigned char cmddamP2_C1;
-unsigned char cmddamP2_C2;
-unsigned char cmddamP2_C3;
-unsigned char cmddamP2_C4;
-unsigned char cmddamP2_C5;
-unsigned char cmddamP2_C6;
-
-unsigned char cmddamP3_C1;
-unsigned char cmddamP3_C2;
-unsigned char cmddamP3_C3;
-unsigned char cmddamP3_C4;
-unsigned char cmddamP3_C5;
-unsigned char cmddamP3_C6;
-
-unsigned char cmddamP4_C1;
-unsigned char cmddamP4_C2;
-unsigned char cmddamP4_C3;
-unsigned char cmddamP4_C4;
-unsigned char cmddamP4_C5;
-unsigned char cmddamP4_C6;
-
-unsigned char cmddamP5_C1;
-unsigned char cmddamP5_C2;
-unsigned char cmddamP5_C3;
-unsigned char cmddamP5_C4;
-unsigned char cmddamP5_C5;
-unsigned char cmddamP5_C6;
-
-unsigned char cmddamP6_C1;
-unsigned char cmddamP6_C2;
-unsigned char cmddamP6_C3;
-unsigned char cmddamP6_C4;
-unsigned char cmddamP6_C5;
-unsigned char cmddamP6_C6;
+unsigned int playerValues[8][10]; //[player][value]
 
 //===================================== SETUP =====================================
 void setup() {
@@ -117,9 +54,9 @@ void setup() {
   arduboy.clear();
 
   arduboy.setFrameRate(10); //not sure if this is needed or should be different for this game
-  
+
   //make sure we start out with buttons available to be pressed
-  buttonAvailable = 1; 
+  buttonAvailable = 1;
 
   state = PLAYERSELECTION;
 
@@ -129,180 +66,223 @@ void setup() {
 
 void reset() {
 
-selectionPlayer = SEL_PLAYER1;
-selectionValue = SEL_LIFE;
-selectionDigit = SEL_ONES;
+  selectionPlayer = SEL_PLAYER1;
+  selectionValue = SEL_LIFE;
 
-lifeP1 = 40;
-lifeP2 = 40;
-lifeP3 = 40;
-lifeP4 = 40;
-lifeP5 = 40;
-lifeP6 = 40;
-
-poisonP1 = 0;
-poisonP2 = 0;
-poisonP3 = 0;
-poisonP4 = 0;
-poisonP5 = 0;
-poisonP6 = 0;
-
-expP1 = 0;
-expP2 = 0;
-expP3 = 0;
-expP4 = 0;
-expP5 = 0;
-expP6 = 0;
-
-cmddamP1_C1 = 0;
-cmddamP1_C2 = 0;
-cmddamP1_C3 = 0;
-cmddamP1_C4 = 0;
-cmddamP1_C5 = 0;
-cmddamP1_C6 = 0;
-cmddamP2_C1 = 0;
-cmddamP2_C2 = 0;
-cmddamP2_C3 = 0;
-cmddamP2_C4 = 0;
-cmddamP2_C5 = 0;
-cmddamP2_C6 = 0;
-cmddamP3_C1 = 0;
-cmddamP3_C2 = 0;
-cmddamP3_C3 = 0;
-cmddamP3_C4 = 0;
-cmddamP3_C5 = 0;
-cmddamP3_C6 = 0;
-cmddamP4_C1 = 0;
-cmddamP4_C2 = 0;
-cmddamP4_C3 = 0;
-cmddamP4_C4 = 0;
-cmddamP4_C5 = 0;
-cmddamP4_C6 = 0;
-cmddamP5_C1 = 0;
-cmddamP5_C2 = 0;
-cmddamP5_C3 = 0;
-cmddamP5_C4 = 0;
-cmddamP5_C5 = 0;
-cmddamP5_C6 = 0;
-cmddamP6_C1 = 0;
-cmddamP6_C2 = 0;
-cmddamP6_C3 = 0;
-cmddamP6_C4 = 0;
-cmddamP6_C5 = 0;
-cmddamP6_C6 = 0;
+  for (char w = 0; w < 8; w++) {
+    for (char h = 0; h < 8; h++) {
+      playerValues[w][h] = 0; //commander damage
+    }
+    playerValues[w][8] = 40; //life
+    playerValues[w][9] = 0; //poison
+  }
 
 }
 
 void draw() {
+
   //21x8 characters
-//=======================
-//= P1-40  P2-40  P3-40 =  select first - use *      6,0
-//= P4-40  P5-40  P6-40 =                            6,9
-//----------------------=                            6,19
-//= Lf-40  Po-00  Ex-00 =  select second - use *     6,21 
-//= C1-00  C2-00  C3-00 =                            6,30
-//= C4-00  C5-00  C6-00 =                            6,39
-//----------------------=                            6,49
-//= Set Value: 00       =  entry - use underline     6,51
-//=======================
+  //=======================
+  //=  P1   P2   P3   P4  =
+  //=  P5   P6   P7   P8  =
+  //=---------------------=
+  //= Life-040  Poison-00 =
+  //= Comm1-40  Comm2-40  =
+  //= Comm3-40  Comm4-40  =
+  //= Comm5-40  Comm6-40  =
+  //= Comm7-40  Comm8-40  =
+  //=======================
 
-//always draw top selection area
-arduboy.setCursor(6, 0);
-arduboy.print("P1-");
-arduboy.print(lifeP1);
+  //always draw top selection area
+  arduboy.setCursor(12, 2); //2
+  arduboy.print("P1");
 
-arduboy.setCursor(54, 0);
-arduboy.print("P2-");
-arduboy.print(lifeP2);
+  arduboy.setCursor(42, 2); //7
+  arduboy.print("P2");
 
-arduboy.setCursor(96, 0);
-arduboy.print("P3-");
-arduboy.print(lifeP3);
+  arduboy.setCursor(72, 2); //12
+  arduboy.print("P3");
 
-arduboy.setCursor(6, 8);
-arduboy.print("P4-");
-arduboy.print(lifeP4);
+  arduboy.setCursor(102, 2); //17
+  arduboy.print("P4");
 
-arduboy.setCursor(54, 8);
-arduboy.print("P5-");
-arduboy.print(lifeP5);
+  arduboy.setCursor(12, 10); //2
+  arduboy.print("P5");
 
-arduboy.setCursor(96, 8);
-arduboy.print("P6-");
-arduboy.print(lifeP6);
+  arduboy.setCursor(42, 10); //7
+  arduboy.print("P6");
 
-arduboy.drawLine(0, 9, 0, 128, WHITE);
+  arduboy.setCursor(72, 10); //12
+  arduboy.print("P7");
 
-switch (selectionPlayer) {
+  arduboy.setCursor(102, 10); //17
+  arduboy.print("P8");
+
+  arduboy.drawRect(0, 0, 128, 64, WHITE);
+  arduboy.drawLine(0, 18, 128, 18, WHITE);
+  arduboy.drawLine(0, 28, 128, 28, WHITE);
+  
+  switch (selectionPlayer) {
     case SEL_PLAYER1 :
-      arduboy.setCursor(0, 0);
+      arduboy.setCursor(6, 2);
       break;
     case SEL_PLAYER2 :
-      arduboy.setCursor(48, 0);
+      arduboy.setCursor(36, 2);
       break;
     case SEL_PLAYER3 :
-      arduboy.setCursor(90, 0);
+      arduboy.setCursor(66, 2);
       break;
     case SEL_PLAYER4 :
-      arduboy.setCursor(0, 8);
+      arduboy.setCursor(96, 2);
       break;
     case SEL_PLAYER5 :
-      arduboy.setCursor(48, 8);
+      arduboy.setCursor(6, 10);
       break;
     case SEL_PLAYER6 :
-      arduboy.setCursor(90, 8);
+      arduboy.setCursor(36, 10);
+      break;
+    case SEL_PLAYER7 :
+      arduboy.setCursor(66, 10);
+      break;
+    case SEL_PLAYER8 :
+      arduboy.setCursor(96, 10);
       break;
   } //state
   arduboy.print(">");
 
-if (state == VALUESELECTION || state == VALUEENTRY)
-  //draw middle selection
 
+  arduboy.setCursor(12, 20);
+  arduboy.print("Life-");
+  arduboy.print(playerValues[selectionPlayer][8]);
 
-  if (state == VALUEENTRY) {
-    //draw value entry
-  
+  arduboy.setCursor(72, 20);
+  arduboy.print("Poison-");
+  arduboy.print(playerValues[selectionPlayer][9]);
+
+  arduboy.setCursor(12, 30);
+  arduboy.print("Cmdr1-");
+  arduboy.print(playerValues[selectionPlayer][0]);
+
+  arduboy.setCursor(72, 30);
+  arduboy.print("Cmdr2-");
+  arduboy.print(playerValues[selectionPlayer][1]);
+
+  arduboy.setCursor(12, 38);
+  arduboy.print("Cmdr3-");
+  arduboy.print(playerValues[selectionPlayer][2]);
+
+  arduboy.setCursor(72, 38);
+  arduboy.print("Cmdr4-");
+  arduboy.print(playerValues[selectionPlayer][3]);
+
+  arduboy.setCursor(12, 46);
+  arduboy.print("Cmdr5-");
+  arduboy.print(playerValues[selectionPlayer][4]);
+
+  arduboy.setCursor(72, 46);
+  arduboy.print("Cmdr6-");
+  arduboy.print(playerValues[selectionPlayer][5]);
+
+  arduboy.setCursor(12, 54);
+  arduboy.print("Cmdr7-");
+  arduboy.print(playerValues[selectionPlayer][6]);
+
+  arduboy.setCursor(72, 54);
+  arduboy.print("Cmdr8-");
+  arduboy.print(playerValues[selectionPlayer][7]);
+
+  //21x8 characters
+  //=======================
+  //=  P1   P2   P3   P4  =
+  //=  P5   P6   P7   P8  =
+  //=---------------------=
+  //= Life-040  Poison-00 =
+  //= Comm1-40  Comm2-40  =
+  //= Comm3-40  Comm4-40  =
+  //= Comm5-40  Comm6-40  =
+  //= Comm7-40  Comm8-40  =
+  //=======================
+
+  if (state == VALUESELECTION) {
+    //draw middle selection
+    
+
+    switch (selectionValue) {
+      case SEL_LIFE :
+        arduboy.setCursor(6, 20);
+        break;
+      case SEL_POISON :
+        arduboy.setCursor(66, 20);
+        break;
+      case SEL_COMMANDER1 :
+        arduboy.setCursor(6, 30);
+        break;
+      case SEL_COMMANDER2 :
+        arduboy.setCursor(66, 30);
+        break;
+      case SEL_COMMANDER3 :
+        arduboy.setCursor(6, 38);
+        break;
+      case SEL_COMMANDER4 :
+        arduboy.setCursor(66, 38);
+        break;
+      case SEL_COMMANDER5 :
+        arduboy.setCursor(6, 46);
+        break;
+      case SEL_COMMANDER6 :
+        arduboy.setCursor(66, 46);
+        break;
+      case SEL_COMMANDER7 :
+        arduboy.setCursor(6, 54);
+        break;
+      case SEL_COMMANDER8 :
+        arduboy.setCursor(66, 54);
+        break;
+    } //state
+    arduboy.print(">");
+
   }
+
 }
 
 
 //===================================== LOOP =====================================
 void loop() {
 
-  if(!arduboy.nextFrame()) {
+  if (!arduboy.nextFrame()) {
     return;
   }
-  
+
   arduboy.clear();
 
-    if (arduboy.pressed(UP_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      up();
-    }
-    if (arduboy.pressed(DOWN_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      down();
-    }
-    if (arduboy.pressed(LEFT_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      left();
-    }
-    if (arduboy.pressed(RIGHT_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      right();
-    }
-    if (arduboy.pressed(A_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      a();
-    }
-    if (arduboy.pressed(B_BUTTON) && buttonAvailable == 1) {
-      buttonAvailable = 0;
-      b();
-    }
+  if (arduboy.pressed(UP_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    up();
+  }
+  if (arduboy.pressed(DOWN_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    down();
+  }
+  if (arduboy.pressed(LEFT_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    left();
+  }
+  if (arduboy.pressed(RIGHT_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    right();
+  }
+  if (arduboy.pressed(A_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    a();
+  }
+  if (arduboy.pressed(B_BUTTON) && buttonAvailable == 1) {
+    buttonAvailable = 0;
+    b();
+  }
 
-    draw();
+  draw();
 
-    if (buttonAvailable == 0) {
+  if (buttonAvailable == 0) {
     if (!arduboy.pressed(A_BUTTON) && !arduboy.pressed(B_BUTTON) && !arduboy.pressed(UP_BUTTON) && !arduboy.pressed(DOWN_BUTTON) && !arduboy.pressed(LEFT_BUTTON) && !arduboy.pressed(RIGHT_BUTTON)) {
       buttonAvailable = 1;
     }
@@ -317,13 +297,36 @@ void up() {
 
   switch (state) {
     case PLAYERSELECTION :
-      //
+      if (selectionPlayer == SEL_PLAYER5) {
+        selectionPlayer = SEL_PLAYER1;
+      } else  if (selectionPlayer == SEL_PLAYER6) {
+        selectionPlayer = SEL_PLAYER2;
+      } else if (selectionPlayer == SEL_PLAYER7) {
+        selectionPlayer = SEL_PLAYER3;
+      } else if (selectionPlayer == SEL_PLAYER8) {
+        selectionPlayer = SEL_PLAYER4;
+      }
       break;
+
     case VALUESELECTION :
-      //
-      break;
-    case VALUEENTRY :
-      //
+      if (selectionValue == SEL_COMMANDER1) {
+        selectionValue = SEL_LIFE;
+      } else  if (selectionValue == SEL_COMMANDER2) {
+        selectionValue = SEL_POISON;
+      } else  if (selectionValue == SEL_COMMANDER3) {
+        selectionValue = SEL_COMMANDER1;
+      } else if (selectionValue == SEL_COMMANDER5) {
+        selectionValue = SEL_COMMANDER3;
+      } else  if (selectionValue == SEL_COMMANDER7) {
+        selectionValue = SEL_COMMANDER5;
+      } else  if (selectionValue == SEL_COMMANDER4) {
+        selectionValue = SEL_COMMANDER2;
+      } else  if (selectionValue == SEL_COMMANDER6) {
+        selectionValue = SEL_COMMANDER4;
+      } else  if (selectionValue == SEL_COMMANDER8) {
+        selectionValue = SEL_COMMANDER6;
+      }
+
       break;
   } //state
 
@@ -333,14 +336,39 @@ void down() {
 
   switch (state) {
     case PLAYERSELECTION :
-      //
+      if (selectionPlayer == SEL_PLAYER1) {
+        selectionPlayer = SEL_PLAYER5;
+      } else if (selectionPlayer == SEL_PLAYER2) {
+        selectionPlayer = SEL_PLAYER6;
+      } else if (selectionPlayer == SEL_PLAYER3) {
+        selectionPlayer = SEL_PLAYER7;
+      } else if (selectionPlayer == SEL_PLAYER4) {
+        selectionPlayer = SEL_PLAYER8;
+      }
+
       break;
+      
     case VALUESELECTION :
-      //
+      if (selectionValue == SEL_LIFE) {
+        selectionValue = SEL_COMMANDER1;
+      } else  if (selectionValue == SEL_POISON) {
+        selectionValue = SEL_COMMANDER2;
+      } else  if (selectionValue == SEL_COMMANDER1) {
+        selectionValue = SEL_COMMANDER3;
+      } else if (selectionValue == SEL_COMMANDER3) {
+        selectionValue = SEL_COMMANDER5;
+      } else  if (selectionValue == SEL_COMMANDER5) {
+        selectionValue = SEL_COMMANDER7;
+      } else  if (selectionValue == SEL_COMMANDER2) {
+        selectionValue = SEL_COMMANDER4;
+      } else  if (selectionValue == SEL_COMMANDER4) {
+        selectionValue = SEL_COMMANDER6;
+      } else  if (selectionValue == SEL_COMMANDER6) {
+        selectionValue = SEL_COMMANDER8;
+      }
+
       break;
-    case VALUEENTRY :
-      //
-      break;
+
   } //state
 
 }
@@ -349,14 +377,37 @@ void left() {
 
   switch (state) {
     case PLAYERSELECTION :
-      //
+      if (selectionPlayer == SEL_PLAYER2) {
+        selectionPlayer = SEL_PLAYER1;
+      } else if (selectionPlayer == SEL_PLAYER3) {
+        selectionPlayer = SEL_PLAYER2;
+      } else if (selectionPlayer == SEL_PLAYER4) {
+        selectionPlayer = SEL_PLAYER3;
+      } else if (selectionPlayer == SEL_PLAYER6) {
+        selectionPlayer = SEL_PLAYER5;
+      } else if (selectionPlayer == SEL_PLAYER7) {
+        selectionPlayer = SEL_PLAYER6;
+      } else if (selectionPlayer == SEL_PLAYER8) {
+        selectionPlayer = SEL_PLAYER7;
+      }
+
       break;
+      
     case VALUESELECTION :
-      //
+      if (selectionValue == SEL_POISON) {
+        selectionValue = SEL_LIFE;
+      } else  if (selectionValue == SEL_COMMANDER2) {
+        selectionValue = SEL_COMMANDER1;
+      } else if (selectionValue == SEL_COMMANDER4) {
+        selectionValue = SEL_COMMANDER3;
+      } else  if (selectionValue == SEL_COMMANDER6) {
+        selectionValue = SEL_COMMANDER5;
+      } else  if (selectionValue == SEL_COMMANDER8) {
+        selectionValue = SEL_COMMANDER7;
+      }
+
       break;
-    case VALUEENTRY :
-      //
-      break;
+
   } //state
 
 }
@@ -365,14 +416,37 @@ void right() {
 
   switch (state) {
     case PLAYERSELECTION :
-      //
+      if (selectionPlayer == SEL_PLAYER1) {
+        selectionPlayer = SEL_PLAYER2;
+      } else if (selectionPlayer == SEL_PLAYER2) {
+        selectionPlayer = SEL_PLAYER3;
+      } else if (selectionPlayer == SEL_PLAYER3) {
+        selectionPlayer = SEL_PLAYER4;
+      } else if (selectionPlayer == SEL_PLAYER5) {
+        selectionPlayer = SEL_PLAYER6;
+      } else if (selectionPlayer == SEL_PLAYER6) {
+        selectionPlayer = SEL_PLAYER7;
+      } else if (selectionPlayer == SEL_PLAYER7) {
+        selectionPlayer = SEL_PLAYER8;
+      }
+
       break;
+      
     case VALUESELECTION :
-      //
+      if (selectionValue == SEL_LIFE) {
+        selectionValue = SEL_POISON;
+      } else  if (selectionValue == SEL_COMMANDER1) {
+        selectionValue = SEL_COMMANDER2;
+      } else  if (selectionValue == SEL_COMMANDER3) {
+        selectionValue = SEL_COMMANDER4;
+      } else if (selectionValue == SEL_COMMANDER5) {
+        selectionValue = SEL_COMMANDER6;
+      } else  if (selectionValue == SEL_COMMANDER7) {
+        selectionValue = SEL_COMMANDER8;
+      }
+
       break;
-    case VALUEENTRY :
-      //
-      break;
+
   } //state
 
 }
@@ -381,14 +455,12 @@ void a() {
 
   switch (state) {
     case PLAYERSELECTION :
-      //
+      state = VALUESELECTION;
       break;
     case VALUESELECTION :
-      //
+
       break;
-    case VALUEENTRY :
-      //
-      break;
+
   } //state
 
 }
@@ -400,11 +472,9 @@ void b() {
       //
       break;
     case VALUESELECTION :
-      //
+      state = PLAYERSELECTION;
       break;
-    case VALUEENTRY :
-      //
-      break;
+
   } //state
 
 }
